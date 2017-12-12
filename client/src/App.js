43fe5aa782b50f5e7b6d42e592978c6161da55f3
WPH2017 <2974769672@qq.com>
2017-12-13 03:47:21 +0800

    init the react-based code-snippets

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,56 @@
 import React, { Component } from 'react';
-import {Button} from 'antd'
-import logo from './logo.svg';
 import './App.css';
+import Pick from './components/pick'
+import { Layout, Row, Col, Input, Menu, Dropdown, Avatar, Icon } from 'antd';
+const Search = Input.Search;
+const { Header, Footer, Content } = Layout;
+const menu = (
+    <Menu>
+      <Menu.Item>
+        <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">您</a>
+      </Menu.Item>
+      <Menu.Item>
+        <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">账户设置</a>
+      </Menu.Item>
+      <Menu.Item>
+        <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">退出</a>
+      </Menu.Item>
+    </Menu>
+);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button type="primary">按钮</Button>
-      </div>
+        <Layout>
+          <Header>
+            <Row type="flex" justify="center">
+              <Col span={4} style={{ fontSize: 20 }}>
+                <Icon type="code-o" /> Code Snippets
+              </Col>
+              <Col span={4}></Col>
+              <Col span={4}></Col>
+              <Col span={4} id="account">
+                <Search
+                    placeholder="输入搜索字段"
+                    onSearch={value => console.log(value)}
+                    enterButton
+                    style={{ width : 200, marginRight:10 }}
+                />
+                <Dropdown overlay={menu} placement="bottomLeft">
+                  <Avatar icon="user" />
+                </Dropdown>
+              </Col>
+            </Row>
+          </Header>
+          <Content>
+            <Row type="flex" justify="center">
+              <Col span={16}>
+                <Pick></Pick>
+              </Col>
+            </Row>
+          </Content>
+          <Footer>Author: WengPenghui Date: 2017/12/13</Footer>
+        </Layout>
     );
   }
 }
---
+++ client/src/components/pick.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Pick extends Component {
+  render () {
+    return (
+        <h1>内容</h1>
+    )
+  }
+}
+
+export default Pick
