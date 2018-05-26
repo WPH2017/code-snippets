b0cbdb95786960b9f4930b5653d4b21d2cc5eac5
WPH <wengpenghui@abcpen.com>
2018-05-26 11:48:02 +0800

    feat(server): basic config(30%)

--- server/app.js
+++ server/app.js
@@ -1,15 +1,29 @@
-const Koa=require('koa')
-const app=new Koa()
+const Koa = require('koa');
+const app = new Koa();
 
-app.use(async (ctx,next)=>{
-  const start=Date.now()
-  await next()
-  const ms=Date.now() - start
-  ctx.set('X-Response-time',`${ms}ms`)
-})
+// x-response-time
 
-app.use(async (ctx,next)=>{
+app.use(async (ctx, next) => {
+    const start = Date.now();
+    await next();
+    const ms = Date.now() - start;
+    ctx.set('X-Response-Time', `${ms}ms`);
+});
 
-})
+// logger
 
-app.listen(3000)
+app.use(async (ctx, next) => {
+    const start = Date.now();
+    await next();
+    const ms = Date.now() - start;
+    console.log(ctx)
+    console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
+
+// response
+
+app.use(async ctx => {
+    ctx.body = 'Hello World';
+});
+
+app.listen(3000);
---
+++ server/config/index.js
@@ -0,0 +1,11 @@
+const pro = {
+    
+};
+
+const dev = {
+    db: 'http://localhost:27017'
+};
+
+let env = dev;
+
+module.exports = env;
